d7db5ea8582973975ac5bb8cb7d7bc31314aaed2
David Butler <dwbutler@ucla.edu>
2017-11-10 08:14:20 -0800

    Replace deprecated alias_method_chain with Module#prepend (Rails 5.1 compatibility)

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -5,16 +5,15 @@ module FaceCrop
       ActiveSupport.on_load :active_record do
            
         class ActiveRecord::Base
-          
-          class << self
-            def has_attached_file_with_face_crop_cache(name, args)
-              has_attached_file_without_face_crop_cache(name, args)
+          module Paperclip::FaceCrop::WithCache
+            def has_attached_file(name, args)
+              super(name, args)
               send("after_#{name}_post_process", lambda { FaceCrop::Detector::Cache.clear })
             end
-            
-            alias_method_chain :has_attached_file, :face_crop_cache
           end
-        end        
+
+          prepend Paperclip::FaceCrop::WithCache
+        end
       end
       
     end
